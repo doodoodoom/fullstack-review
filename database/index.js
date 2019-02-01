9db621ba92ea33ccd9d279822c770b583f8cc742
doodoodoom <madridmatt17@gmail.com>
2019-02-01 14:06:40 -0800

    Define repoSchema in database/index.js, bind onChange to Search component

--- database/index.js
+++ database/index.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   // we're connected!
+// });
+
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  username: String,
+  html_url: String,
+  forks_count: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
